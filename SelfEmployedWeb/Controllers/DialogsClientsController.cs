d9c8d7e5e85d4543a3848f11ac49f2ee427143bb
danila-permogorsky <97134335+danila-permogorsky@users.noreply.github.com>
2022-08-15 21:48:42 +0300

    Deleted unnecessary classes, added required

---
+++ SelfEmployedWeb/Controllers/DialogsClientsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SelfEmployedWeb.Models;
+
+namespace SelfEmployedWeb.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DialogsClientsController : ControllerBase
+{
+	private readonly ILogger<DialogsClientsController> _logger;
+
+	public DialogsClientsController(ILogger<DialogsClientsController> logger)
+	{
+		_logger = logger;
+	}
+
+	[HttpGet("getDialogGuidByClients/{clients}")]
+	public Guid GetDialogGuidByClients(string clients)
+	{
+		if (string.IsNullOrEmpty(clients)) return Guid.Empty;
+
+		var clientsList = clients
+			.Trim()
+			.Split(" ")
+			.Select(Guid.Parse)
+			.ToList();
+
+		var dialogGuid = new RGDialogsClients().Init()
+			.GroupBy(d => d.IDRGDialog)
+			.Where(gr => clientsList.All(c => gr.Any(dial => dial.IDClient == c)))
+			.Select(gr => gr.Key)
+			.FirstOrDefault();
+		
+		return 
+			string.IsNullOrEmpty(dialogGuid.ToString())
+				? Guid.Empty 
+				: dialogGuid;
+	}
+}
